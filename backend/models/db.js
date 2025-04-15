0a93553a0a60765cd4d74a6858b1eaedb5b10755
Ignacio-Subiabre <igsubiabre@alumnos.uai.cl>
2025-04-15 12:34:00 -0400

    Creación DB en Mongo DB y conexión con backend

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,3 +1,15 @@
-//cd P01-Grupo-10-TICS420\backend\models
-//node descargar_pdfs_node.js
-//prueba de commit link con jira 2
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://ignaciosubiabre10:iasc159753@cluster0.tw1gdvo.mongodb.net/empresas?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, '❌ Error de conexión a MongoDB Atlas:'));
+db.once('open', () => {
+  console.log('✅ Conectado a MongoDB Atlas');
+});
+
+module.exports = db;
